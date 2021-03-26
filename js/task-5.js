3a86daaf682e6ca29fa6244ea410c44f561f02af
OlehFilyak <73895897+OlehFilyak@users.noreply.github.com>
2021-03-26 21:54:02 +0200

    add answer for task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,4 +1,22 @@
-// Напиши скрипт який, при наборі тексту в інпут input#name-input (подія input), підставляє його поточне значення в span#name-output. якщо інпут порожній, в спані повинен відображатися рядок 'незнайомець'.
+// Напиши скрипт який, при наборі тексту в інпут input#name-input (подія input), 
+// підставляє його поточне значення в span#name - output.якщо інпут порожній, в спані повинен відображатися рядок 'незнайомець'.
 
-// <input type="text" placeholder="Ваше ім'я?" id="name-input" />
-// <h1>Привіт, <span id="name-output">незнайомець</span>!</h1>
+    // <input type="text" placeholder="Ваше ім'я?" id="name-input" />
+    // <h1>Привіт, <span id="name-output">незнайомець</span>!</h1>
+
+// Answer
+
+const refs = { // Отримуємо посилання
+    nameInput: document.querySelector('#name-input'),
+    nameOutput: document.querySelector('#name-output'),
+}
+
+
+const changeNameGuest = () => { // Через те, що стрілка, то пишемо її перед слухачем події (не може бути виклик перед ініціалізацією)
+
+ refs.nameInput.value === '' ? refs.nameOutput.textContent = 'Незнайомець' : refs.nameOutput.textContent = refs.nameInput.value; // Порівнюємо із пестою строкою
+}
+
+refs.nameInput.addEventListener('input', changeNameGuest) // реєструємо слухача події, викликаємо callback
+
+// console.log(refs.nameInput.value === '')  Тест
